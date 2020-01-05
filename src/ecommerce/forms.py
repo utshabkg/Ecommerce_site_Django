88c5b9284bc87b9941d9f3d51408ee504c95f60b
utshab-1603022 <ranjanyouth@gmail.com>
2020-01-06 00:05:15 +0600

    First commit

---
+++ src/ecommerce/forms.py
@@ -0,0 +1,37 @@
+from django import forms
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+class ContactForm(forms.Form):
+    fullname = forms.CharField(
+            widget=forms.TextInput(
+                    attrs={
+                        "class": "form-control",
+                        "placeholder": "Your full name"
+                    }
+                    )
+            )
+    email    = forms.EmailField(
+            widget=forms.EmailInput(
+                    attrs={
+                        "class": "form-control",
+                        "placeholder": "Your email"
+                    }
+                    )
+            )
+    content  = forms.CharField(
+            widget=forms.Textarea(
+                attrs={
+                    'class': 'form-control',
+                    "placeholder": "Your message"
+                    }
+                )
+            )
+
+    def clean_email(self):
+        email = self.cleaned_data.get("email")
+        if not "gmail.com" in email:
+            raise forms.ValidationError("Email has to be gmail.com")
+        return email
+
